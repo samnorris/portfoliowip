56d8b6f6667a085abe1706e2ea00ff0d2855af8a
Sam <theperfectpixels@gmail.com>
2017-03-07 04:48:20 +1300

    //gallery code cleanup
    next task: sass code cleanup in work.scss

--- js/main.js
+++ js/main.js
@@ -667,7 +667,6 @@ $(document).ready(function() {
 ///////////////////////////////////////////////////////////////////////////
 
 	$("#mainMenu a,#mainheader a").click(function() {
-		$("#menu-container").removeClass('open');
 		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 			var target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
